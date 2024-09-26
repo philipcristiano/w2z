f1fe1614332b25f40700f1152f926f2a0390d32a
Philip Cristiano <git@philipcristiano.com>
2024-09-26 10:30:27 -0400

    chore: Clean up flake
    Postgres/atlas/foreman are not needed

--- flake.nix
+++ flake.nix
@@ -21,11 +21,8 @@
                     extensions = [ "rust-src" "rustfmt" ];
                 })
                 rust-analyzer
-                pkgs.postgresql_16
                 pkgs.tailwindcss
-                pkgs.foreman
                 pkgs.openssl # native-tls is included in cargo, needs work to remove
-                pkgs.atlas
             ] ++
               pkgs.lib.optionals pkgs.stdenv.isDarwin [
                 darwin.apple_sdk.frameworks.Security # Should only be for darwin
@@ -33,8 +30,6 @@
             ]
             ;
             shellHook = ''
-              export PGDATA=$PWD/pgdata
-              export DATABASE_URL="postgres://timeline@localhost/timeline?sslmode=disable"
             '';
           };
         }
